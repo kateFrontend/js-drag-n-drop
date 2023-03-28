07ff8a81a5d4c0a657da94beb6aea5b27e96eec0
kateFrontend <ecdanilova@gmail.com>
2023-03-28 09:54:29 -0400

    setup functions

---
+++ script.js
@@ -0,0 +1,36 @@
+const fill = document.querySelector('.fill')
+const empties = document.querySelectorAll('.empty')
+
+fill.addEventListener('dragstart', dragStart)
+fill.addEventListener('dragend', dragEnd)
+
+for(const empty of empties) {
+    empty.addEventListener('dragover', dragOver)
+    empty.addEventListener('dragenter', dragEnter)
+    empty.addEventListener('dragleave', dragLeave)
+    empty.addEventListener('drop', dragDrop)
+}
+
+function dragStart() {
+    console.log('drag start')
+}
+
+function dragEnd() {
+    console.log('drag end')
+}
+
+function dragOver() {
+    console.log('drag over')
+}
+
+function dragEnter() {
+    console.log('drag enter')
+}
+
+function dragLeave() {
+    console.log('drag leave')
+}
+
+function dragDrop() {
+    console.log('drag drop')
+}
